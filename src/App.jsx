791813feb6342c1528bf8efd153d3d219490371e
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-06-09 15:41:30 +0200

    Part 7.8 - Implement ultimate hook

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,21 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    console.log("Fetching data")
+    axios
+      .get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios
+      .post(baseUrl, resource)
+      .then(response => {
+        setResources(resources.concat(response.data))
+      })
   }
 
   const service = {
